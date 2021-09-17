67bf79ba13e9fda84698b3d724f56603926997d5
lost731 <lost731@naver.com>
2021-09-17 23:43:57 +0900

    feat(Home) Home Design and make login_form

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
+import { Input, Menu, Row, Col } from "antd";
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-      <Link href="/">
-        <a>투게더</a>
-      </Link>
-      <Link href="/profile">
-        <a>프로필</a>
-      </Link>
-      <Link href="/signup">
-        <a>회원가입</a>
-      </Link>
-      {children}
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>투게더</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://lost731.github.io"
+            target="_black"
+            rel="noreferrer noopener"
+          >
+            made by KimNaMu
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
@@ -22,4 +53,5 @@ const AppLayout = ({ children }) => {
 AppLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
 export default AppLayout;
