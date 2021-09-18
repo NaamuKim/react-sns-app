de70844d28e4b92b5319bd7e5a1c82b13e1e7d63
lost731 <lost731@naver.com>
2021-09-19 01:59:07 +0900

    feat(presentor): add presentor of sign-up and user-profile

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,15 +1,29 @@
 import React from "react";
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
+  const followerList = [
+    { nickname: "김지원" },
+    { nickname: "윤소희" },
+    { nickname: "단풍이" },
+  ];
+  const followingList = [
+    { nickname: "김지원" },
+    { nickname: "윤소희" },
+    { nickname: "단풍이" },
+  ];
   return (
     <>
       <Head>
         <title>내 프로필 | Together</title>
       </Head>
       <AppLayout>
-        <div>내 프로필</div>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
       </AppLayout>
     </>
   );
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,17 +1,107 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import AppLayout from "../components/AppLayout";
 import Head from "next/head";
+import { Button, Checkbox, Form, Input } from "antd";
+import useInput from "../hooks/useInput";
+import styled from "styled-components";
 
+const ErrorMessage = styled.div`
+  color: darkred;
+`;
+
+const SubmitWrapper = styled.div`
+  margin-top: 10px;
+`;
 const Signup = () => {
+  const [id, onChangeId] = useInput("");
+  const [nickname, onChangeNickname] = useInput("");
+  const [password, onChangePassword] = useInput("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+    },
+    [password]
+  );
+  const [term, setTerm] = useState(false);
+  const [termError, setTermError] = useState(false);
+  const onChangeTerm = useCallback((e) => {
+    setTerm(e.target.checked);
+    setTermError(false);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!term) {
+      return setTermError(true);
+    }
+    console.log(id, nickname, password);
+  }, [password, passwordCheck, term]);
   return (
-    <>
+    <AppLayout>
       <Head>
         <title>회원가입 | Together</title>
       </Head>
-      <AppLayout>
-        <div>회원가입 페이지</div>
-      </AppLayout>
-    </>
+      <Form onFinish={onSubmit}>
+        <div>
+          <label htmlFor="user-id">아이디</label>
+          <br />
+          <Input name="user-id" value={id} onChange={onChangeId} required />
+        </div>
+        <div>
+          <label htmlFor="user-nickname">닉네임</label>
+          <br />
+          <Input
+            name="user-nickname"
+            value={nickname}
+            onChange={onChangeNickname}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password">패스워드</label>
+          <br />
+          <Input
+            name="user-password"
+            type="password"
+            value={password}
+            onChange={onChangePassword}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="user-check-password">패스워드 확인</label>
+          <br />
+          <Input
+            name="user-check-password"
+            type="password"
+            value={passwordCheck}
+            onChange={onChangePasswordCheck}
+            required
+          />
+          {passwordError && (
+            <ErrorMessage>비밀번호가 일치하지 않습니다.</ErrorMessage>
+          )}
+        </div>
+        <div>
+          <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+            이용약관 확인 후 동의합니다.
+          </Checkbox>
+          {termError && (
+            <ErrorMessage>약관 동의는 필수사항입니다.</ErrorMessage>
+          )}
+        </div>
+        <SubmitWrapper>
+          <Button type="primary" htmlType="submit">
+            가입하기
+          </Button>
+        </SubmitWrapper>
+      </Form>
+    </AppLayout>
   );
 };
 
