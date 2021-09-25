dac7f2fedbbf1ef8c1b603ebf8c68e072ff169c3
lost731 <lost731@naver.com>
2021-09-25 15:43:00 +0900

    ADD: Back-End

---
+++ back/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const postRouter = require("./routes/post");
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.send("hello api");
+});
+
+app.get("/posts", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      content: "hello",
+    },
+    {
+      id: 2,
+      content: "hello2",
+    },
+    {
+      id: 3,
+      content: "hello3",
+    },
+  ]);
+});
+
+app.use("/post", postRouter);
+
+app.listen(3000, () => {
+  console.log("서버 실행 중");
+});
